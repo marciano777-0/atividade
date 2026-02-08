69a2b8f183fc9ebfcbe4b374e80dd5365aa591a0
marciano777-0 <marciopiccoli47@gmail.com>
2026-02-08 19:02:43 -0300

    Add files via upload

---
+++ atividade/torneio.js
@@ -0,0 +1,10 @@
+class flamengo{ 
+
+constructor(gols,vitorias,derrotas){
+this.gols=gols
+this.vitorias=vitorias
+this.derrotas=derrotas
+     }
+ }
+ let flamengo= new flamengo(7,5,3)
+ console.log(flamengo)
